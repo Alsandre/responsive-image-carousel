21a1fcfbb8a3ef6384a693800baf0311c28d342a
Aleksandre <gabeditebuli@gmail.com>
2023-03-11 14:56:19 +0400

    fix: build errors

--- src/layout/OptionLayout.tsx
+++ src/layout/OptionLayout.tsx
@@ -1,25 +1,10 @@
-import { ChangeEvent, ReactEventHandler, useContext, useState, MouseEvent } from 'react';
-
-import { SliderContext } from '../context/slider-context';
 
 import Option from '../components/options/Option';
 
 import classes from './OptionLayout.module.css';
 
 export default function OptionLayout () : JSX.Element {
-    const {sliderState, setSliderState} = useContext(SliderContext);
 
-    console.log(sliderState);
-
-    const modeSelectHandler = (event : ChangeEvent<HTMLSelectElement>) => {
-        switch(event.target.value) {
-            case 'Baseline controls':
-                setSliderState(prevState => {
-                    return {...prevState, controls: event.target.value}
-                })
-        }
-        console.log(event.currentTarget);
-    };
     
     return <div className={classes.menu}>
         <Option variant='Control layout' options={['Lower line', 'Mid line']}/>

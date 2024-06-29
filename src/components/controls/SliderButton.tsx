654daf8aeae5ebc6a291a29c2474941a6b4145f1
alsandre <gabeditebuli@gmail.com>
2024-06-29 16:53:06 +0400

    cleanup: remove deprecated code

--- src/components/controls/SliderButton.tsx
+++
@@ -1,9 +0,0 @@
-type TProps = {
-    className: string,
-    onClick: () => void,
-    direction: string
-}
-
-export default function SliderButton (props : TProps) : JSX.Element {
-    return <button className={props.className} onClick={props.onClick}>{props.direction}</button>
-}
